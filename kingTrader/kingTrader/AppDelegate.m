340579594eb4b7a0c25ff090bae12d26f6b29bc4
zhangyihao_personnal <15003125615@163.com>
2016-04-06 10:41:00 +0800

    test

--- kingTrader/kingTrader/AppDelegate.m
+++ kingTrader/kingTrader/AppDelegate.m
@@ -20,7 +20,7 @@
  *  创建根视图控制器
  */
 - (void)createRootViewController{
-    
+    KTLog(@"00000");
     // 状态栏显示白色 配合info.plist View controller-based status bar appearance NO
     [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
